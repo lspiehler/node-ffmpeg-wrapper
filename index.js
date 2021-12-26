528cab59685f76567848be753c6a4dd26799fecb
Lyas Spiehler <lspiehler@gmail.com>
2021-12-26 15:43:15 -0600

    make input an array and add encoder

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const { spawn } = require( 'child_process' );
+//const { StringDecoder } = require('string_decoder');
+const EventEmitter = require('events');
 var bindir;
 
 var normalizeCommand = function(command) {
@@ -22,6 +24,215 @@ var normalizeCommand = function(command) {
     return outcmd;
 }
 
+var binCommand = function() {
+    const stdoutbuff = [];
+    const stderrbuff = [];
+    var code;
+    var exited = false;
+    var stdoutrecvd = false;
+    var stderrrecvd = true;
+    var exec;
+    var path;
+    var command;
+    var callback;
+
+    var handleExit = function() {
+        var out = {
+            command: path + ' ' + command.join(' '),
+            stdout: Buffer.concat(stdoutbuff),
+            stderr: Buffer.concat(stderrbuff),
+            exitcode: code
+        }
+        if (code != 0) {
+            callback(Buffer.concat(stderrbuff).toString(), out);
+        } else {
+            callback(false, out);
+        }
+    }
+
+    this.write = function(stdin) {
+        exec.stdin.write(stdin);
+    }
+
+    this.run = function(params, rtio, endcallback) {
+        path = params.bin;
+        callback = endcallback;
+
+        if(bindir) {
+            path = bindir + '/' + path;
+        }
+
+        command = normalizeCommand(params.cmd);
+        if(params.hasOwnProperty('in')) {
+            for(let i = 0; i < params.in.length; i++) {
+                command.unshift(params.in);
+                command.unshift('-i');
+            }
+        }
+
+        if(params.hasOwnProperty('out')) {
+            //for(let i = 0; i < params.in.length; i++) {
+                command.push(params.out);
+                //command.unshift('-i');
+            //}
+        }
+
+        try {
+            exec = spawn( path, command);
+    
+            if(params.hasOwnProperty('stdin')) {
+                if(params.stdin) {
+                    exec.stdin.write(params.stdin);
+                    //console.log(params.stdin);
+                    //exec.stdin.end();
+                }
+            }
+    
+            exec.stdout.on('data', function(data) {
+                //console.log(data.toString());
+                stdoutrecvd = true;
+                if(rtio) {
+                    rtio(data);
+                    stdoutbuff[0] = data;
+                } else {
+                    stdoutbuff.push(data);
+                }
+                if(exited && code == 0) {
+                    handleExit();
+                }
+            });
+    
+            exec.on('error', function(err) {
+                //console.log(err);
+                callback(err, false);
+                return;
+            });
+    
+            exec.stderr.on('data', function(data) {
+                stderrrecvd = true;
+                stderrbuff.push(data);
+                if(exited && code != 0) {
+                    handleExit();
+                }
+            });
+    
+            exec.on('exit', function(ecode) {
+                exited = true;
+                code = ecode;
+                if(stdoutrecvd && ecode == 0) {
+                    handleExit();
+                }
+    
+                if(stderrrecvd && ecode != 0) {
+                    handleExit();
+                }
+                
+            });
+        } catch(e) {
+            callback(e, false);
+            return;
+        }
+    }
+}
+
+var probe = function(params, callback) {
+    var bin = 'ffprobe';
+    var cmd = ['-print_format json -show_format -show_streams'];
+    var probes = [];
+    var index = 0;
+
+    var loopProbe = function(callback) {
+        let command = new binCommand();
+        command.run({ cmd: cmd.join(' '), bin: bin, in: [params.in[index]]}, false, function(err, out) {
+            //console.log(out);
+            if(err) {
+                callback(err, false);
+            } else {
+                //out.stdout = out.stdout.toString();
+                //out.stderr = out.stderr.toString();
+                probes.push({
+                    probe: JSON.parse(out.stdout.toString()),
+                    cmd: out.command
+                });
+                index++;
+                if(index < params.in.length) {
+                    loopProbe(callback);
+                } else {
+                    callback(false, probes);
+                }
+            }
+        });
+    }
+
+    loopProbe(function(err, out, cmd) {
+        callback(err, out, cmd);
+    });
+}
+
+class encoder extends EventEmitter {
+
+    constructor() {
+        super();
+        this.command;
+    }
+
+    sendCommand(cmd) {
+        if(this.command) {
+            this.command.write(cmd);
+            return 0;
+        } else {
+            return 'encoder isn\'t running yet'
+        }
+    }
+
+    stop() {
+        if(this.command) {
+            this.command.write('q');
+            return 0;
+        } else {
+            return 'encoder isn\'t running yet'
+        }
+    }
+
+    start(params, callback) {
+        //const em = new EventEmitter();
+        var self = this;
+        let bin = 'ffmpeg';
+        callback(self);
+        new probe({in: params.in}, function(err, inputs) {
+            if(err) {
+                self.emit('error', err);
+            } else {
+                let duration = inputs[0].probe.format.duration * 1000;
+                let cmd = ['-y -map 0:0 -map 0:1 -c:v:0 copy -c:a:0 aac -ac 2 -progress -'];
+                self.command = new binCommand();
+                self.command.run({ cmd: cmd.join(' '), bin: bin, in: params.in, out: params.out},
+                function(stdout) {
+                    /*if(index++ == 20) {
+                        command.write('q');
+                    }*/
+                    let lines = stdout.toString().split('\n');
+                    let progress = {};
+                    for(let i = 0; i < lines.length - 1; i++) {
+                        let kv = lines[i].split('=');
+                        progress[kv[0]] = kv[1];
+                    }
+                    progress.percent_complete = Math.round((progress.out_time_ms) / duration * 10) / 100;
+                    self.emit('progress', progress);
+                },
+                function(err, out) {
+                    //console.log(out);
+                    if(err) {
+                        self.emit('error', err);
+                    } else {
+                        self.emit('success', out);
+                    }
+                });
+            }
+        });
+    }
+}
+
 module.exports = function(options) {
 	
 	if(options) {
@@ -30,102 +241,17 @@ module.exports = function(options) {
 		}
     }
 
-    var runCommand = function(params, callback) {
-		const stdoutbuff = [];
-		const stderrbuff = [];
-		var code;
-		var exited = false;
-		var stdoutrecvd = false;
-		var stderrrecvd = true;
-
-        let path = params.bin;
-
-        if(bindir) {
-            path = bindir + '/' + path;
-        }
-
-        var command = normalizeCommand(params.cmd);
-        if(params.hasOwnProperty('in')) {
-            command.unshift(params.in);
-            command.unshift('-i');
-        }
-
-        //console.log(command.join(' '));
-
-        var handleExit = function() {
-            var out = {
-                command: path + ' ' + command.join(' '),
-                stdout: Buffer.concat(stdoutbuff),
-                stderr: Buffer.concat(stderrbuff),
-                exitcode: code
-            }
-            if (code != 0) {
-                callback(Buffer.concat(stderrbuff).toString(), out);
-            } else {
-                callback(false, out);
-            }
-        }
-
-        try {
-            const exec = spawn( path, command);
-
-            if(params.hasOwnProperty('stdin')) {
-                if(params.stdin) {
-                    exec.stdin.write(params.stdin);
-                    //console.log(params.stdin);
-                    exec.stdin.end();
-                }
-            }
-
-            exec.stdout.on('data', function(data) {
-                stdoutrecvd = true;
-                stdoutbuff.push(data);
-                if(exited && code == 0) {
-                    handleExit();
-                }
-            });
-
-            exec.on('error', function(err) {
-                //console.log(err);
-                callback(err, false);
-                return;
-            });
-
-            exec.stderr.on('data', function(data) {
-                stderrrecvd = true;
-                stderrbuff.push(data);
-                if(exited && code != 0) {
-                    handleExit();
-                }
-            });
-
-            exec.on('exit', function(ecode) {
-                exited = true;
-                code = ecode;
-                if(stdoutrecvd && ecode == 0) {
-                    handleExit();
-                }
-
-                if(stderrrecvd && ecode != 0) {
-                    handleExit();
-                }
-                
-            });
-        } catch(e) {
-            callback(e, false);
-			return;
-        }
+    this.probe = function(params, callback) {
+        new probe(params, function(err, out, cmd) {
+            callback(err, out)
+        });
     }
 
-    this.probe = function(params, callback) {
-        let bin = 'ffprobe';
-        let cmd = ['-v quiet -print_format json -show_format -show_streams']
-        runCommand({ cmd: cmd.join(' '), bin: 'ffprobe', in: params.path}, function(err, out) {
-			if(err) {
-				callback(err, false, out.command);
-			} else {
-				callback(false, JSON.parse(out.stdout.toString()), out.command);
-			}
-		});
+    this.encode = function(params, callback) {
+        let encode = new encoder();
+        //console.log(encode.stop());
+        encode.start(params, function(encoder) {
+            callback(encoder);
+        });
     }
 }
